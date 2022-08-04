8909b74ce86a99d93e72c69f7f18f6332737c8ce
Mak Jones <jonesmakalyn@gmail.com>
2022-08-03 21:26:23 -0400

    22.2 exercises

---
+++ unit22-python-fundamentals/22.2-exercise/exercises/capitalize.py
@@ -0,0 +1,12 @@
+def capitalize(phrase):
+    """Capitalize first letter of first word of phrase.
+
+        >>> capitalize('python')
+        'Python'
+
+        >>> capitalize('only first word')
+        'Only first word'
+    """
+    first = phrase[0].upper()
+    rest = phrase[1:]
+    print(first + rest)
---
+++ unit22-python-fundamentals/22.2-exercise/exercises/flip_case.py
@@ -0,0 +1,21 @@
+def flip_case(phrase, to_swap):
+    """Flip [to_swap] case each time it appears in phrase.
+
+        >>> flip_case('Aaaahhh', 'a')
+        'aAAAhhh'
+
+        >>> flip_case('Aaaahhh', 'A')
+        'aAAAhhh'
+
+        >>> flip_case('Aaaahhh', 'h')
+        'AaaaHHH'
+
+    """
+    
+    new_phrase = [ltr.lower() if ltr == to_swap.upper() else ltr.upper() if ltr == to_swap.lower() else ltr for ltr in phrase]
+
+    return ''.join(new_phrase)
+
+print(flip_case('Aaaahhh', 'a'))
+print(flip_case('Aaaahhh', 'A'))
+print(flip_case('Aaaahhh', 'h'))

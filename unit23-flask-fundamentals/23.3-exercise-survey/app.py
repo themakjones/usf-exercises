ebaa09d7471db53c7f09ce01552631c91e79034b
Mak Jones <jonesmakalyn@gmail.com>
2022-08-16 16:50:17 -0400

    23.3 survey exercise

---
+++ unit23-flask-fundamentals/23.3-exercise-survey/app.py
@@ -0,0 +1,45 @@
+from flask import Flask, request, render_template, redirect, flash
+from flask_debugtoolbar import DebugToolbarExtension
+from surveys import satisfaction_survey
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret'
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+
+
+
+responses = []
+curr_question = 0
+
+@app.route('/')
+def start_page():
+    survey_title = satisfaction_survey.title
+    instructions = satisfaction_survey.instructions
+
+    return render_template("start.html", survey_title=survey_title, instructions=instructions)
+
+@app.route('/question/<int:question_num>')
+def start_survey(question_num):
+    if not question_num == curr_question:
+        flash('You have tried to acces an invalid question. Please answer the current question to continue.')
+        return redirect(f'/question/{curr_question}')
+    else:
+        if len(responses) < len(satisfaction_survey.questions):
+            question = satisfaction_survey.questions[question_num].question
+            choices = satisfaction_survey.questions[question_num].choices
+            return render_template('question.html', question=question,choices=choices)
+        else:
+            return redirect('/thank-you')
+
+@app.route('/answer', methods=["POST"])
+def add_answer():
+    global curr_question 
+    answer = request.form['option']
+    responses.append(answer)
+    curr_question += 1
+    return redirect(f'/question/{curr_question}')
+
+@app.route('/thank-you')
+def say_thanks():
+    # return render_template('thanks.html')
+    return responses

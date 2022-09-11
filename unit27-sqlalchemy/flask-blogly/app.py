704f9a12b437377194d025028c529e011834fd36
Mak Jones <jonesmakalyn@gmail.com>
2022-09-11 12:53:31 -0400

    basic routes and models created

--- unit27-sqlalchemy/flask-blogly/app.py
+++ unit27-sqlalchemy/flask-blogly/app.py
@@ -1,12 +1,29 @@
 """Blogly application."""
 
-from flask import Flask
-from models import db, connect_db
+from flask import Flask, redirect, request, render_template
+from models import db, connect_db, User
+from flask_debugtoolbar import DebugToolbarExtension
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'dillondainssucks'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 
+debug = DebugToolbarExtension(app)
+
 connect_db(app)
 db.create_all()
+
+@app.route('/')
+def home_page():
+    """Home page"""
+
+    return render_template('home.html')
+
+@app.route('/add-user')
+def add_user():
+    """Add user page"""
+    return render_template('new_user.html')
+
+    

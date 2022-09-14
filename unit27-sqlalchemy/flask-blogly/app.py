1752c87e580437043464747d9093ca98c9860df0
Mak Jones <jonesmakalyn@gmail.com>
2022-09-13 20:17:33 -0400

    Added routes and templates

--- unit27-sqlalchemy/flask-blogly/app.py
+++ unit27-sqlalchemy/flask-blogly/app.py
@@ -1,11 +1,12 @@
 """Blogly application."""
 
-from flask import Flask, redirect, request, render_template
+from flask import Flask, redirect, request, render_template, flash
 from models import db, connect_db, User
 from flask_debugtoolbar import DebugToolbarExtension
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'dillondainssucks'
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
@@ -19,11 +20,47 @@ db.create_all()
 def home_page():
     """Home page"""
 
-    return render_template('home.html')
+    return redirect('/users')
 
-@app.route('/add-user')
+@app.route('/users')
+def users_page():
+    """Lists all users"""
+
+    users = User.query.all()
+
+    return render_template('users.html', users=users)
+
+@app.route('/users/new', methods=['POST', 'GET'])
 def add_user():
-    """Add user page"""
+    """Add user page and process add user form"""
+
+    if request.method == 'POST':
+        first_name = request.form['first-name']
+        last_name = request.form['last-name']
+        img_url = request.form['img-url']
+        img_url = img_url if img_url else None
+
+        new_user = User(first_name=first_name, last_name=last_name, image_url=img_url)
+        db.session.add(new_user)
+        db.session.commit()
+        full_name = new_user.get_full_name()
+
+        flash(f"New user: {full_name} added!")
+        return redirect('/users')
+
     return render_template('new_user.html')
+
+@app.route('/users/<int:user_id>')
+def user_profile_page(user_id):
+    """User profile page"""
+
+    user = User.query.get_or_404(user_id)
+    return render_template('user_profile.html', user=user)
+
+@app.route('/users/<int:user_id>/edit', methods=['POST', 'GET'])
+def edit_user(user_id):
+    user = User.query.get_or_404(user_id)
+    return render_template('edit_user.html', user=user)
+
 
     
--- unit27-sqlalchemy/flask-blogly/models.py
+++ unit27-sqlalchemy/flask-blogly/models.py
@@ -18,4 +18,11 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     first_name = db.Column(db.String(25), nullable=False)
     last_name = db.Column(db.String(25), nullable=False)
-    image_url = db.Column(db.String)
+    image_url = db.Column(db.String, default='/static/default_img.jpg')
+
+    def get_full_name(self):
+        """ Gets user's full name """
+
+        full_name = ' '.join([self.first_name, self.last_name])
+        return full_name
+

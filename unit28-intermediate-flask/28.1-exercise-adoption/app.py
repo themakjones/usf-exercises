aec31a25456579b933f0c4d25504dd0c814d77aa
Mak Jones <jonesmakalyn@gmail.com>
2022-09-22 06:54:55 -0400

    added form model and additional routes

--- unit28-intermediate-flask/28.1-exercise-adoption/app.py
+++ unit28-intermediate-flask/28.1-exercise-adoption/app.py
@@ -1,8 +1,7 @@
 from flask import Flask, redirect, request, render_template, flash
 from models import Pet, connect_db, db
-from flask_wtf import FlaskForm
-from wtforms import StringField
 from flask_debugtoolbar import DebugToolbarExtension
+from forms import AddPetForm
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'jY*j9t79Jhf8'
@@ -16,11 +15,11 @@ debug = DebugToolbarExtension(app)
 connect_db(app)
 db.create_all()
 
-@app.errorhandler(404)
-def page_not_found(e):
-    """Renders custom 404 page when there is a 404 error"""
+# @app.errorhandler(404)
+# def page_not_found(e):
+#     """Renders custom 404 page when there is a 404 error"""
     
-    return render_template('404.html'), 404
+#     return render_template('404.html'), 404
 
 @app.route('/')
 def home_page():
@@ -31,7 +30,35 @@ def home_page():
 
 @app.route('/add', methods=['GET', 'POST'])
 def add_pet():
-    """Add pet to adoption list"""
+    """Add pet to adoption list form, handle adding"""
+
+    form = AddPetForm()
 
     if form.validate_on_submit():
+        name = form.name.data
+        species = form.species.data
+        breed = form.breed.data
+        photo_url = form.photo_url.data
+        age = form.age.data
+        notes = form.notes.data
+
+        new_pet = Pet(name=name, species=species, breed=breed, photo_url=photo_url, age=age, notes=notes)
+
+        db.session.add(new_pet)
+        db.session.commit()
+
+        flash(f"Added {name} to adoption list!")
+        return redirect('/')
+
+
+    return render_template('add_pet.html', form=form)
+
+@app.route('/<int:pet_id>')
+def pet_page(pet_id):
+    """Pet detail page"""
+
+    pet = Pet.query.get_or_404(pet_id)
+    
+    return render_template('pet_page.html', pet=pet)
+
 
--- unit28-intermediate-flask/28.1-exercise-adoption/forms.py
+++ unit28-intermediate-flask/28.1-exercise-adoption/forms.py
@@ -1,8 +1,13 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField
+from wtforms import StringField, SelectField, FloatField
+from wtforms.validators import InputRequired, URL, NumberRange, Optional
 
 class AddPetForm(FlaskForm):
     """Form for adding pet"""
 
-    name = StringField('Name')
-    species = SelectField('Species', choices=[('dog', 'Dog'), ('cat', 'Cat'), ('dragon', 'Bearded Dragon')])
+    name = StringField('Name', validators=[InputRequired(message=['Name is required'])])
+    species = SelectField('Species', choices=[('dog', 'Dog'), ('cat', 'Cat'), ('dragon', 'Bearded Dragon')], validators=[InputRequired(message=['Please select the species'])])
+    breed = StringField('Breed')
+    photo_url = StringField('Image Link', validators=[URL(message=['Link must in the the form of a URL'])])
+    age = FloatField('Age', validators=[Optional(), NumberRange(min=0, max=30, message=['Please enter an age between 0 and 30'])])
+    notes = StringField('Notes')

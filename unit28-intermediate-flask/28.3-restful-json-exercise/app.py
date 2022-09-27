b1555c5f1a8944c97e66018361346fbfaa7aebdd
Mak Jones <jonesmakalyn@gmail.com>
2022-09-27 17:53:02 -0400

    Get and Post routes created

--- unit28-intermediate-flask/28.3-restful-json-exercise/app.py
+++ unit28-intermediate-flask/28.3-restful-json-exercise/app.py
@@ -1,6 +1,6 @@
 """Flask app for Cupcakes"""
 
-from flask import Flask, redirect, request, render_template, flash
+from flask import Flask, redirect, request, render_template, flash, jsonify
 from models import Cupcake, connect_db, db
 from flask_debugtoolbar import DebugToolbarExtension
 
@@ -15,3 +15,35 @@ debug = DebugToolbarExtension(app)
 
 connect_db(app)
 db.create_all()
+
+@app.route('/api/cupcakes')
+def list_cupcakes():
+    """List all cupcakes"""
+
+    cupcakes = Cupcake.query.all()
+    serialized = [cupcake.serialize() for cupcake in cupcakes]
+
+    return jsonify(cupcakes=serialized)
+
+@app.route('/api/cupcakes/<int:id>')
+def show_cupcake(id):
+    """Shows cupcake based on id"""
+
+    cupcake = Cupcake.query.get_or_404(id)
+
+    return jsonify(cupcake=cupcake.serialize())
+
+@app.route("/api/cupcakes", methods=['POST'])
+def create_cupcake():
+    """Creates a new cupcake in database"""
+
+    flavor = request.json["flavor"]
+    size = request.json["size"]
+    rating = request.json["rating"]
+    image = request.json["image"]
+
+    new_cupcake = Cupcake(flavor=flavor, size=size, rating=rating, image=image)
+    db.session.add(new_cupcake)
+    db.session.commit()
+
+    return (jsonify(cupcake=new_cupcake.serialize()), 201)

5b29ce51f384ccff40721846c46bb8e8d95affc7
Mak Jones <jonesmakalyn@gmail.com>
2022-09-27 16:33:47 -0400

    Created models and serialize function, updated reqs

--- unit28-intermediate-flask/28.3-restful-json-exercise/app.py
+++ unit28-intermediate-flask/28.3-restful-json-exercise/app.py
@@ -1,1 +1,17 @@
 """Flask app for Cupcakes"""
+
+from flask import Flask, redirect, request, render_template, flash
+from models import Cupcake, connect_db, db
+from flask_debugtoolbar import DebugToolbarExtension
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'jY*j9t79Jhf8'
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///mj_adoption'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+
+debug = DebugToolbarExtension(app)
+
+connect_db(app)
+db.create_all()
--- unit28-intermediate-flask/28.3-restful-json-exercise/models.py
+++ unit28-intermediate-flask/28.3-restful-json-exercise/models.py
@@ -1,1 +1,42 @@
 """Models for Cupcake app."""
+
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import relationship
+
+db = SQLAlchemy()
+
+def connect_db(app):
+    """Connect to database"""
+
+    db.app = app
+    db.init_app(app)
+
+Default_Img = 'https://tinyurl.com/demo-cupcake'
+
+class Cupcake(db.Model):
+    """Cupcake model"""
+
+    __tablename__ = 'cupcakes'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    flavor = db.Column(db.String(25), nullable=False)
+    size = db.Column(db.String(5), nullable=False)
+    rating = db.Column(db.Float, nullable=False)
+    image = db.Column(db.String, nullable=False, default=Default_Img)
+
+    def __repr__(self):
+        """Show cupcake info"""
+
+        return f"<Cupcake {self.id} {self.flavor} {self.size}>"
+
+    def serialize(self):
+        """Serialize cupcake object to dictionary"""
+
+        return {
+            "id": self.id,
+            "flavor": self.flavor,
+            "size": self.size,
+            "rating": self.rating,
+            "image": self.image
+        }
+

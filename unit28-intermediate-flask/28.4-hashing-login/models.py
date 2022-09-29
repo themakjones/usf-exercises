7fa41e4fbde1313ab840ef0a66f7294dad948cc2
Mak Jones <jonesmakalyn@gmail.com>
2022-09-29 17:20:29 -0400

    Added register and authenticate methods

--- unit28-intermediate-flask/28.4-hashing-login/models.py
+++ unit28-intermediate-flask/28.4-hashing-login/models.py
@@ -3,8 +3,46 @@ from flask_bcrypt import Bcrypt
 
 db = SQLAlchemy()
 
+bcrypt = Bcrypt()
+
 def connect_db(app):
     """Connect to database"""
 
     db.app = app
     db.init_app(app)
+
+
+class User(db.Model):
+    """User model"""
+
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(20), nullable=False, unique=True)
+    password = db.Column(db.Text, nullable=False)
+    email = db.Column(db.String(50), nullable=False)
+    first_name = db.Column(db.String(30), nullable=False)
+    last_name = db.Column(db.String(30), nullable=False)
+
+    @classmethod
+    def register(cls, username, pwd, email, first_name, last_name):
+        """Register user w/ a hashed password and return the user"""
+
+        hashed = bcrypt.generate_password_hash(pwd)
+        hashed_utf8 = hashed.decode("utf8")
+
+        return cls(username=username, password=hashed_utf8, email=email, first_name=first_name, last_name=last_name)
+
+    @classmethod
+    def authenticate(cls, username, pwd):
+        """Validate user and check if password is correct
+
+        Return user if valid, else return False
+        """
+
+        user = User.query.filter_by(username=username).first()
+
+        if user and bcrypt.check_password_hash(user.password, pwd):
+            return user
+        else:
+            return False

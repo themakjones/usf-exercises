9ba45998b6d5287ae90dcdb03d1b47f9762b04b6
Mak Jones <jonesmakalyn@gmail.com>
2022-09-30 15:27:02 -0400

    Requirements updated

--- unit28-intermediate-flask/28.4-hashing-login/app.py
+++ unit28-intermediate-flask/28.4-hashing-login/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, redirect, session, flash
-from flask_debugtoolbar import DebugToolbarExtention
+from flask_debugtoolbar import DebugToolbarExtension
 from models import User, connect_db, db
 
 app = Flask(__name__)
@@ -9,7 +9,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///flask_feedback'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 
-debug = DebugToolbarExtention(app)
+debug = DebugToolbarExtension(app)
 
 connect_db(app)
 db.create_all()

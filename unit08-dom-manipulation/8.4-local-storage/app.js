5792d2210ce5fad18f5c1a5161fc768935a1689b
themakjones <106115468+themakjones@users.noreply.github.com>
2022-12-30 13:54:36 -0500

    Fixed array lists and remove button

--- unit08-dom-manipulation/8.4-local-storage/app.js
+++ unit08-dom-manipulation/8.4-local-storage/app.js
@@ -6,15 +6,22 @@ const form = document.querySelector(`form`);
     // Add To Do item (does not save on reload)
 form.addEventListener(`submit`, function(e) {
     e.preventDefault();
-    let newToDo = document.querySelector(`#addToDo`);
+    let newToDo = document.querySelector(`#addToDo`).value;
     let newLi = document.createElement(`li`);
     let removeButton = document.createElement(`button`);
+    let newDiv = document.createElement('div');
 
-    newLi.innerText = newToDo.value;
+    newDiv.innerText = newToDo;
+    newDiv.classList.add('todo-item')
+
+    newLi.append(newDiv);
     newLi.append(removeButton);
     ul.append(newLi);
     removeButton.innerText = `Remove`;
-    toDoList.push(newToDo.value);
+    toDoList.push(newToDo);
+
+    console.log(`todo list: ${toDoList}`)
+    console.log(`completed list: ${completedList}`)
 
     form.reset();
 })
@@ -23,13 +30,49 @@ form.addEventListener(`submit`, function(e) {
 
 ul.addEventListener(`click`, function(e) {
     const targetTag = e.target.tagName.toLowerCase();
-    
-    if (targetTag === `li`) {
-        e.target.className = `completed`;
-        completedList.push(newToDo.value);
-        // above line gives error bc newToDo is not defined
+
+    if (e.target.classList.contains('todo-item')) {
+
+        checkCompletion(e.target);
+
     } else if (targetTag === `button`) {
+
+        const todo = e.target.previousElementSibling.innerText;
+        
+        removeItem(toDoList, todo);
+        removeItem(completedList, todo);
         e.target.parentNode.remove();
     }
+
+    console.log(`todo list: ${toDoList}`)
+    console.log(`completed list: ${completedList}`)
 })
+
+const checkCompletion = (target) => {
+
+    if (target.classList.contains('completed')) {
+    
+        removeItem(completedList, target.innerText);
+        toDoList.push(target.innerText);
+        
+        target.classList.remove('completed');
+
+    } else {
+    
+        removeItem(toDoList, target.innerText);
+        completedList.push(target.innerText);
+        
+        target.classList.add('completed');
+    }
+}
+
+const removeItem = (arr, val) => {
+    const idx = arr.indexOf(val);
+
+    if (idx > -1) {
+        arr.splice(idx, 1);
+
+        return arr;
+    }
+}
 // add items to local storage

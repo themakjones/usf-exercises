3c27c8a85360abb526d631f6ed73d30c379892f5
Mak Jones <jonesmakalyn@gmail.com>
2023-02-11 16:13:51 -0500

    makeText function

--- unit33-node-fundamentals/33.5-testing-jest/markov.js
+++ unit33-node-fundamentals/33.5-testing-jest/markov.js
@@ -14,6 +14,10 @@ class MarkovMachine {
    *  for text of "the cat in the hat", chains will be
    *  {"the": ["cat", "hat"], "cat": ["in"], "in": ["the"], "hat": [null]} */
 
+  static randomWord(arr) {
+    return arr[Math.floor(Math.random() * arr.length)];
+  }
+
   makeChains() {
     // TODO
     let chain = {};
@@ -28,23 +32,31 @@ class MarkovMachine {
       // add word to chain object and map positions to next word
       if (!(`${word}` in chain)) {
         chain[word] = positionsOfWord.map((position) => {
-          return this.words[position + 1];
+          return position < this.words.length - 1
+            ? this.words[position + 1]
+            : null;
         });
       }
     });
 
-    return chain;
+    this.chains = chain;
   }
 
   /** return random text from chains */
 
   makeText(numWords = 100) {
     // TODO
+    let keys = Object.keys(this.chains);
+    let key = MarkovMachine.randomWord(keys);
+    let output = [];
+
+    while (output.length < numWords && key !== null) {
+      output.push(key);
+      key = MarkovMachine.randomWord(this.chains[key]);
+    }
+
+    return output.join(" ");
   }
 }
-
-let mm = new MarkovMachine("the cat in the hat");
-
-console.log(mm.makeChains());
 
 module.exports = {MarkovMachine};
